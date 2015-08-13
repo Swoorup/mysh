319bd14fc4bc091f7b94362d4a06c9592ff3feb3
Swoorup Joshi <swoorupj@gmail.com>
2015-08-14 00:16:06 +0800

    Fixed Formatting

--- shell.c
+++ shell.c
@@ -13,52 +13,52 @@ int main()
 	// ignore Ctrl-\ Ctrl-C Ctrl-Z signals
 	ignore_signal_for_shell();
 
-    // set the prompt
-    set_prompt("swoorup % ");
+	// set the prompt
+	set_prompt("swoorup % ");
 
-    while (1)
-    {
+	while (1)
+	{
 		char* linebuffer;
 		size_t len;
-		
+
 		lexer_t lexerbuf;
 		ASTreeNode* exectree;
 
 		// keep getline in a loop in case interruption occurs
-        int again = 1;
-        while (again) {
-            again = 0;
-            printf("%s", getprompt());
+		int again = 1;
+		while (again) {
+			again = 0;
+			printf("%s", getprompt());
 			linebuffer = NULL;
 			len = 0;
 			ssize_t nread = getline(&linebuffer, &len, stdin);
-            if (nread <= 0 && errno == EINTR) {
-                again = 1;        	// signal interruption, read again
+			if (nread <= 0 && errno == EINTR) {
+				again = 1;        	// signal interruption, read again
 				clearerr(stdin);	// clear the error
 			}
-        }
-		
+		}
+
 		// user pressed ctrl-D
-        if ( feof(stdin) ) {
+		if (feof(stdin)) {
 			exit(0);
 			return 0;
 		}
-        
-        // lexically analyze and build a list of tokens
-        lexer_build(linebuffer, len, &lexerbuf);
-		
+
+		// lexically analyze and build a list of tokens
+		lexer_build(linebuffer, len, &lexerbuf);
+
 		// parse the tokens into an abstract syntax tree
 		if (!lexerbuf.ntoks || parse(&lexerbuf, &exectree) != 0) {
-            continue;
+			continue;
 		}
-		
+
 		execute_syntax_tree(exectree);
-		
+
 		// free the structures
 		ASTreeNodeDelete(exectree);
 		lexer_destroy(&lexerbuf);
-    }
+	}
 
-    return 0;
+	return 0;
 }
 
